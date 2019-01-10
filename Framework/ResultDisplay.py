776511a0b3dbebdce9402b5a26ecbcea35554cf0
Dengyi <dengyijob@163.com>
2019-01-10 17:11:02 +0800

    时间序列结果重新生成一遍

--- Framework/ResultDisplay.py
+++ Framework/ResultDisplay.py
@@ -39,20 +39,24 @@ def fill_factor_number_info(data, factor_name_dict=None, factor_first_class_dict
 if __name__ == "__main__":
 
     # 测试
-    output_file_url = 'D:/Quantitative_FOF_Framework_data/ResultDisplay_20181228'
+    output_file_url = 'D:/Quantitative_FOF_Framework_data/ResultDisplay_20190110'
 
     factor_list, factor_name_dict, factor_first_class_dict, factor_second_class_dict = get_factor_info()
 
-    # factor_return_reg_columns = ['regression_model', 'rolling_window', 'factor_number', 'type_name', 'get_data_date',
-    #                              'alpha_significance', 'alpha', 'alpha_ste', 'alpha_t_value', 'alpha_p_value',
-    #                              'beta_significance', 'beta', 'beta_ste', 'beta_t_value', 'beta_p_value',
-    #                              'adj_rsquared']
-    #
-    # factor_return_reg_result = read_data_from_oracle_db('select * from lyzs_tinysoft.factor_return_regression')[factor_return_reg_columns]
+    factor_return_reg_columns = ['regression_model', 'rolling_window', 'factor_number', 'type_name', 'get_data_date',
+                                 'alpha_significance', 'alpha', 'alpha_ste', 'alpha_t_value', 'alpha_p_value',
+                                 'beta_significance', 'beta', 'beta_ste', 'beta_t_value', 'beta_p_value',
+                                 'adj_rsquared']
+
+    factor_return_reg_result_sql = 'select ' + ','.join(factor_return_reg_columns) + ' from lyzs_tinysoft.factor_return_regression'
+
+    factor_return_reg_result = read_data_from_oracle_db(factor_return_reg_result_sql).rename(
+        columns={col.upper():col for col in factor_return_reg_columns})[factor_return_reg_columns]
+
     # factor_return_reg_result = pickle.load(open('/Users/yi.deng/Desktop/file/database/factor_return_regression.dat', 'rb'))
 
     critirion = 0.1
-    # signifcant_factor_return_reg = factor_return_reg_result[factor_return_reg_result['alpha_p_value'] <= critirion].copy()
+    signifcant_factor_return_reg = factor_return_reg_result[factor_return_reg_result['alpha_p_value'] <= critirion].copy()
 
     date_sql = '''
         select get_data_date
@@ -68,40 +72,41 @@ if __name__ == "__main__":
                        'beta_significance', 'beta', 'beta_ste', 'beta_t_value', 'beta_p_value',
                        'adj_rsquared']
 
+    # 每个日期下，每个模型、滚动窗口显著情况汇总显著个数
 
-    # # 每个日期下，每个模型、滚动窗口显著情况汇总显著个数
-    #
-    # # 1.1 时间序列
-    #
-    # primary_keys_name_list = ['regression_model', 'rolling_window', 'factor_number']
-    # significant_pk_all_list = [signifcant_factor_return_reg[column_name].unique().tolist() for column_name in primary_keys_name_list]
-    # primary_keys_list = list(itertools.product(*significant_pk_all_list))
-    # week_data_rolling_pct_list = [4, 5, 6, 12, 36, 52]
-    #
-    # factor_return_reg_signifcant_ts_pct = {}
-    #
-    # for primary_key in primary_keys_list:
-    #     temp_ts_pct_pivot_data = pd.DataFrame(index=all_get_data_date, columns=['第' + str(i + 1) + '档收益率' for i in range(10)])
-    #     # 有的档位可能不存在显著的，所以导致该列pivot出来之后不存在
-    #     temp_pivot_data = signifcant_factor_return_reg[signifcant_factor_return_reg[primary_keys_name_list].isin(primary_key).all(1)].pivot_table(
-    #         index='get_data_date', columns='type_name', values='alpha')
-    #     temp_ts_pct_pivot_data.loc[temp_pivot_data.index, temp_pivot_data.columns] = temp_pivot_data
-    #
-    #     temp_ts_pct_melt_data = temp_ts_pct_pivot_data.reset_index().rename(columns={'GET_DATA_DATE': 'get_data_date'}).melt(
-    #         id_vars=['get_data_date'], var_name=['type_name'], value_name='alpha')
-    #
-    #     for rolling_num in week_data_rolling_pct_list:
-    #         rolling_result = temp_ts_pct_melt_data.groupby(by=['type_name']).apply(
-    #             lambda df: df['alpha'].rolling(rolling_num, min_periods=1).apply(lambda alpha: len(pd.Series(alpha).dropna()) / len(alpha))).reset_index()
-    #         temp_ts_pct_melt_data.loc[rolling_result['level_1'], 'last_' + str(rolling_num) + '_week'] = rolling_result['alpha']
-    #
-    #     factor_return_reg_signifcant_ts_pct[primary_key] = temp_ts_pct_melt_data.copy()
-    #     print('1.1 时间序列：' + '，'.join([str(pk) for pk in primary_key]))
-    #
-    # factor_return_reg_signifcant_ts_pct_df = transform_dict_to_df(factor_return_reg_signifcant_ts_pct, primary_keys_name_list)
-    # factor_return_reg_signifcant_ts_pct_df = optimize_data_ram(factor_return_reg_signifcant_ts_pct_df)
-    # pickle_dump_data(factor_return_reg_signifcant_ts_pct_df, output_file_url, data_name='factor_return_reg_signifcant_ts_pct_df')
-    #
+    # 1.1 时间序列
+
+    primary_keys_name_list = ['regression_model', 'rolling_window', 'factor_number']
+    significant_pk_all_list = [signifcant_factor_return_reg[column_name].unique().tolist() for column_name in primary_keys_name_list]
+    primary_keys_list = list(itertools.product(*significant_pk_all_list))
+
+    week_data_rolling_pct_list = [4, 5, 6, 12, 36, 52]
+    factor_return_reg_signifcant_ts_pct = {}
+
+    for primary_key in primary_keys_list:
+        temp_ts_pct_pivot_data = pd.DataFrame(index=all_get_data_date, columns=['第' + str(i + 1) + '档收益率' for i in range(10)])
+        # 有的档位可能不存在显著的，所以导致该列pivot出来之后不存在
+        temp_pivot_data = signifcant_factor_return_reg[signifcant_factor_return_reg[primary_keys_name_list].isin(primary_key).all(1)].pivot_table(
+            index='get_data_date', columns='type_name', values='alpha')
+        temp_ts_pct_pivot_data.loc[temp_pivot_data.index, temp_pivot_data.columns] = temp_pivot_data
+
+        temp_ts_pct_melt_data = temp_ts_pct_pivot_data.reset_index().rename(columns={'GET_DATA_DATE': 'get_data_date'}).melt(
+            id_vars=['get_data_date'], var_name=['type_name'], value_name='alpha')
+
+        for rolling_num in week_data_rolling_pct_list:
+            temp_ts_pct_melt_data['last_' + str(rolling_num) + '_week'] = np.nan
+            rolling_result = temp_ts_pct_melt_data.groupby(by=['type_name']).apply(
+                lambda df: df['alpha'].rolling(rolling_num, min_periods=1).apply(
+                    lambda alpha: len(pd.Series(alpha).dropna()) / len(alpha))).reset_index()
+            temp_ts_pct_melt_data.loc[rolling_result['level_1'], 'last_' + str(rolling_num) + '_week'] = rolling_result['alpha'].values
+
+        factor_return_reg_signifcant_ts_pct[primary_key] = temp_ts_pct_melt_data.copy()
+        print('1.1 时间序列：' + '，'.join([str(pk) for pk in primary_key]))
+
+    factor_return_reg_signifcant_ts_pct_df = transform_dict_to_df(factor_return_reg_signifcant_ts_pct, primary_keys_name_list)
+    factor_return_reg_signifcant_ts_pct_df = optimize_data_ram(factor_return_reg_signifcant_ts_pct_df)
+    pickle_dump_data(factor_return_reg_signifcant_ts_pct_df, output_file_url, data_name='factor_return_reg_signifcant_ts_pct_df')
+
     # # 1.2.1 截面
     # factor_return_reg_signifcant = factor_return_reg_result.groupby(by=[ 'regression_model', 'rolling_window', 'factor_number', 'get_data_date']).apply(
     #     lambda df: sum(df['alpha_p_value'] <= critirion)).reset_index().rename(columns={0: '10档显著个数'})
@@ -123,7 +128,6 @@ if __name__ == "__main__":
     # pickle_dump_data(factor_return_reg_significant_cs_pct, output_file_url, data_name='factor_return_reg_significant_cs_pct')
     #
     # # 2. 基于rank的因子显著性
-    #
     #
     # # 2.1 基于alpha排序的
     #
@@ -210,89 +214,89 @@ if __name__ == "__main__":
     # factor_return_alpha_ranking_consistency_df = transform_dict_to_df(factor_return_alpha_ranking_consistency, primary_keys_name_list)
     # factor_return_alpha_ranking_consistency_df = optimize_data_ram(factor_return_alpha_ranking_consistency_df)
     # pickle_dump_data(factor_return_alpha_ranking_consistency_df, output_file_url, data_name='factor_return_alpha_ranking_consistency_df')
-
-    # 2.2 基于r_squared排序的
-
-    ranking_sql = '''
-        SELECT ''' + ','.join(ranking_columns) + ''',
-            rank () OVER (PARTITION BY REGRESSION_MODEL, ROLLING_WINDOW, GET_DATA_DATE, FACTOR_NUMBER ORDER BY ADJ_RSQUARED DESC) rank
-        FROM LYZS_TINYSOFT.FACTOR_RETURN_REGRESSION
-        WHERE ALPHA_P_VALUE <= 0.1
-    '''
-
-    ranking_rsquared_data = read_data_from_oracle_db(ranking_sql)
-    ranking_rsquared_data.columns = ranking_columns + ['rank']
-    ranking_rsquared_data = ranking_rsquared_data.sort_values(by=['regression_model', 'rolling_window', 'factor_number', 'type_name', 'get_data_date'])
-
-    primary_keys_name_list = ['regression_model', 'rolling_window', 'factor_number']
-    significant_pk_all_list = [ranking_rsquared_data[column_name].unique().tolist() for column_name in primary_keys_name_list]
-    primary_keys_list = list(itertools.product(*significant_pk_all_list))
-
-    factor_number_sql = '''
-        select factor_number
-        from lyzs_tinysoft.factor_library
-        order by factor_number
-    '''
-    factor_number_list = read_data_from_oracle_db(factor_number_sql)['FACTOR_NUMBER']
-    week_data_rolling_pct_list = [12, 24, 52]
-
-    factor_return_rsquared_ranking_consistency = {}
-
-    for primary_key in primary_keys_list:
-
-        # primary_keys：('OLS', 32, 'factor1')
-        temp_rank_pivot_data = pd.DataFrame(index=all_get_data_date, columns=['第' + str(i + 1) + '档收益率' for i in range(10)])
-        # 有的档位可能不存在显著的，所以导致该列pivot出来之后不存在
-        temp_pivot_data = ranking_rsquared_data[ranking_rsquared_data[primary_keys_name_list].isin(primary_key).all(1)].pivot_table(
-                index='get_data_date', columns='type_name', values='rank')
-        temp_rank_pivot_data.loc[temp_pivot_data.index, temp_pivot_data.columns] = temp_pivot_data
-        temp_rank_melt_data = temp_rank_pivot_data.reset_index().rename(columns={'GET_DATA_DATE': 'get_data_date'}).melt(
-            id_vars=['get_data_date'], var_name=['type_name'], value_name='rank')
-
-        for rolling_num in week_data_rolling_pct_list:
-            rolling_rank_number = temp_rank_melt_data.groupby(by=['type_name']).apply(
-                lambda df:
-                df['rank'].rolling(rolling_num, min_periods=1).apply(lambda rolling_data: Counter(pd.Series(rolling_data).dropna()).most_common(1)[0][0])
-            ).reset_index()
-            # Counter(rolling_data).most_common(2) -> [(1, 5), (2, 3)] 第一个tuple表示出现次数最多的数字1出现过的次数是5
-            # Counter(rolling_data).most_common(1)[0][0] 即取出现次数第一多的数字是什么数字
-            # Counter(rolling_data).most_common(1)[0][1] 即取出现次数第一多的数字出现过几次
-            rolling_rank_number_pct = temp_rank_melt_data.groupby(by=['type_name']).apply(
-                lambda df:
-                df['rank'].rolling(rolling_num, min_periods=1).apply(
-                    lambda rolling_data: Counter(pd.Series(rolling_data).dropna()).most_common(1)[0][1] / rolling_num)
-            ).reset_index()
-
-            temp_rank_melt_data.loc[rolling_rank_number['level_1'], 'last_' + str(rolling_num) + '_week_most_freq_rank'] = rolling_rank_number['rank']
-            temp_rank_melt_data.loc[rolling_rank_number_pct['level_1'], 'last_' + str(rolling_num) + '_week_most_freq_rank_pct'] = \
-                rolling_rank_number_pct['rank']
-
-        # 计算一个全时段多最多rank，每个时点往前全时段
-
-        temp_rank_melt_data_cumsum = temp_rank_melt_data.groupby(by=['type_name']).apply(
-            lambda df: pd.get_dummies(df['rank']).cumsum().apply(
-                lambda rank_cumsum: [np.argmax(rank_cumsum), rank_cumsum.max()] if rank_cumsum.max() != 0 else [np.nan, np.nan], axis=1
-            )
-        ).reset_index()
-
-        # 若全都没有显著的则会出现NaN的情况
-        temp_rank_melt_data_cumsum[0] = temp_rank_melt_data_cumsum[0].apply(
-            lambda rank_list: [np.nan, np.nan] if rank_list != rank_list else rank_list)
-
-        # 先算好一个截止到当前的日期长度，作为后面的分母
-        temp_rank_melt_data_cumsum[1] = 1
-        temp_rank_melt_data_date_length = temp_rank_melt_data_cumsum.groupby(by=['type_name']).apply(lambda df: df.cumsum()[1]).reset_index()
-
-        temp_rank_melt_data.loc[temp_rank_melt_data_cumsum['level_1'], 'till_now_most_freq_rank'] = temp_rank_melt_data_cumsum[0].apply(lambda l: l[0])
-        # 注意index对的都是temp_rank_melt_data_cumsum的
-        temp_rank_melt_data.loc[temp_rank_melt_data_cumsum['level_1'], 'till_now_most_freq_rank_pct'] = \
-            temp_rank_melt_data_cumsum[0].apply(lambda l: l[1]).div(temp_rank_melt_data_date_length.loc[temp_rank_melt_data_cumsum['level_1']][1])
-
-        factor_return_rsquared_ranking_consistency[primary_key] = temp_rank_melt_data.copy()
-        print('2.2 基于r_squared排序的：' + '，'.join([str(pk) for pk in primary_key]))
-
-    factor_return_alpha_ranking_consistency_df = transform_dict_to_df(factor_return_rsquared_ranking_consistency, primary_keys_name_list)
-    factor_return_alpha_ranking_consistency_df = optimize_data_ram(factor_return_alpha_ranking_consistency_df)
-    pickle_dump_data(factor_return_alpha_ranking_consistency_df, output_file_url, data_name='factor_return_alpha_ranking_consistency_df')
+    #
+    # # 2.2 基于r_squared排序的
+    #
+    # ranking_sql = '''
+    #     SELECT ''' + ','.join(ranking_columns) + ''',
+    #         rank () OVER (PARTITION BY REGRESSION_MODEL, ROLLING_WINDOW, GET_DATA_DATE, FACTOR_NUMBER ORDER BY ADJ_RSQUARED DESC) rank
+    #     FROM LYZS_TINYSOFT.FACTOR_RETURN_REGRESSION
+    #     WHERE ALPHA_P_VALUE <= 0.1
+    # '''
+    #
+    # ranking_rsquared_data = read_data_from_oracle_db(ranking_sql)
+    # ranking_rsquared_data.columns = ranking_columns + ['rank']
+    # ranking_rsquared_data = ranking_rsquared_data.sort_values(by=['regression_model', 'rolling_window', 'factor_number', 'type_name', 'get_data_date'])
+    #
+    # primary_keys_name_list = ['regression_model', 'rolling_window', 'factor_number']
+    # significant_pk_all_list = [ranking_rsquared_data[column_name].unique().tolist() for column_name in primary_keys_name_list]
+    # primary_keys_list = list(itertools.product(*significant_pk_all_list))
+    #
+    # factor_number_sql = '''
+    #     select factor_number
+    #     from lyzs_tinysoft.factor_library
+    #     order by factor_number
+    # '''
+    # factor_number_list = read_data_from_oracle_db(factor_number_sql)['FACTOR_NUMBER']
+    # week_data_rolling_pct_list = [12, 24, 52]
+    #
+    # factor_return_rsquared_ranking_consistency = {}
+    #
+    # for primary_key in primary_keys_list:
+    #
+    #     # primary_keys：('OLS', 32, 'factor1')
+    #     temp_rank_pivot_data = pd.DataFrame(index=all_get_data_date, columns=['第' + str(i + 1) + '档收益率' for i in range(10)])
+    #     # 有的档位可能不存在显著的，所以导致该列pivot出来之后不存在
+    #     temp_pivot_data = ranking_rsquared_data[ranking_rsquared_data[primary_keys_name_list].isin(primary_key).all(1)].pivot_table(
+    #             index='get_data_date', columns='type_name', values='rank')
+    #     temp_rank_pivot_data.loc[temp_pivot_data.index, temp_pivot_data.columns] = temp_pivot_data
+    #     temp_rank_melt_data = temp_rank_pivot_data.reset_index().rename(columns={'GET_DATA_DATE': 'get_data_date'}).melt(
+    #         id_vars=['get_data_date'], var_name=['type_name'], value_name='rank')
+    #
+    #     for rolling_num in week_data_rolling_pct_list:
+    #         rolling_rank_number = temp_rank_melt_data.groupby(by=['type_name']).apply(
+    #             lambda df:
+    #             df['rank'].rolling(rolling_num, min_periods=1).apply(lambda rolling_data: Counter(pd.Series(rolling_data).dropna()).most_common(1)[0][0])
+    #         ).reset_index()
+    #         # Counter(rolling_data).most_common(2) -> [(1, 5), (2, 3)] 第一个tuple表示出现次数最多的数字1出现过的次数是5
+    #         # Counter(rolling_data).most_common(1)[0][0] 即取出现次数第一多的数字是什么数字
+    #         # Counter(rolling_data).most_common(1)[0][1] 即取出现次数第一多的数字出现过几次
+    #         rolling_rank_number_pct = temp_rank_melt_data.groupby(by=['type_name']).apply(
+    #             lambda df:
+    #             df['rank'].rolling(rolling_num, min_periods=1).apply(
+    #                 lambda rolling_data: Counter(pd.Series(rolling_data).dropna()).most_common(1)[0][1] / rolling_num)
+    #         ).reset_index()
+    #
+    #         temp_rank_melt_data.loc[rolling_rank_number['level_1'], 'last_' + str(rolling_num) + '_week_most_freq_rank'] = rolling_rank_number['rank']
+    #         temp_rank_melt_data.loc[rolling_rank_number_pct['level_1'], 'last_' + str(rolling_num) + '_week_most_freq_rank_pct'] = \
+    #             rolling_rank_number_pct['rank']
+    #
+    #     # 计算一个全时段多最多rank，每个时点往前全时段
+    #
+    #     temp_rank_melt_data_cumsum = temp_rank_melt_data.groupby(by=['type_name']).apply(
+    #         lambda df: pd.get_dummies(df['rank']).cumsum().apply(
+    #             lambda rank_cumsum: [np.argmax(rank_cumsum), rank_cumsum.max()] if rank_cumsum.max() != 0 else [np.nan, np.nan], axis=1
+    #         )
+    #     ).reset_index()
+    #
+    #     # 若全都没有显著的则会出现NaN的情况
+    #     temp_rank_melt_data_cumsum[0] = temp_rank_melt_data_cumsum[0].apply(
+    #         lambda rank_list: [np.nan, np.nan] if rank_list != rank_list else rank_list)
+    #
+    #     # 先算好一个截止到当前的日期长度，作为后面的分母
+    #     temp_rank_melt_data_cumsum[1] = 1
+    #     temp_rank_melt_data_date_length = temp_rank_melt_data_cumsum.groupby(by=['type_name']).apply(lambda df: df.cumsum()[1]).reset_index()
+    #
+    #     temp_rank_melt_data.loc[temp_rank_melt_data_cumsum['level_1'], 'till_now_most_freq_rank'] = temp_rank_melt_data_cumsum[0].apply(lambda l: l[0])
+    #     # 注意index对的都是temp_rank_melt_data_cumsum的
+    #     temp_rank_melt_data.loc[temp_rank_melt_data_cumsum['level_1'], 'till_now_most_freq_rank_pct'] = \
+    #         temp_rank_melt_data_cumsum[0].apply(lambda l: l[1]).div(temp_rank_melt_data_date_length.loc[temp_rank_melt_data_cumsum['level_1']][1])
+    #
+    #     factor_return_rsquared_ranking_consistency[primary_key] = temp_rank_melt_data.copy()
+    #     print('2.2 基于r_squared排序的：' + '，'.join([str(pk) for pk in primary_key]))
+    #
+    # factor_return_alpha_ranking_consistency_df = transform_dict_to_df(factor_return_rsquared_ranking_consistency, primary_keys_name_list)
+    # factor_return_alpha_ranking_consistency_df = optimize_data_ram(factor_return_alpha_ranking_consistency_df)
+    # pickle_dump_data(factor_return_alpha_ranking_consistency_df, output_file_url, data_name='factor_return_alpha_ranking_consistency_df')
 
 
